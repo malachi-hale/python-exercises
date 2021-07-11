7aa1e9aab8656f5ed8d5a364834cd72932ece4f6
malachi-hale <malachi.d.hale@gmail.com>
2021-07-11 13:02:15 -0500

    Completed control structure exercises

--- control_structure_exercises.py
+++ control_structure_exercises.py
@@ -56,4 +56,130 @@ while(i >= 5):
     i -= 5
 
 #2bi 
+user_number = int(input("Please enter a number:  \n"))
 
+for i in range(1,10):
+    product = user_number * i
+    print(str(user_number), "x", str(i), "=", str(product))
+
+#2bii
+for i in range(1, 10):
+    print(str(i) * i)
+
+#2c
+your_number = input("Please enter an odd number between 1 and 50: \n")
+
+while your_number.isdigit() == False: 
+    print("That was not a digit.")
+    your_number = input("Please enter an odd number between 1 and 50: \n")
+
+for i in range(1, 51):
+    if i % 2 == 0 or i == int(your_number):
+        if i == int(your_number):
+            print("Yikes! Skipping number: ", i)
+            continue
+    print("Here is an odd number: ", i)
+
+#2d 
+new_number = input("Please enter a positive number: \n")
+while new_number.isdigit() == False or int(new_number) <= 0: 
+    print("That was not a valid positive number.")
+    your_number = input("Please enter a valid positive number: \n")
+i=0
+while i <= int(new_number):
+    print(i)
+    i +=1
+
+#2e 
+positive_number = input("Please enter a positive integer: ")
+while positive_number.isdigit() == False or int(positive_number) <= 0: 
+    print("That was not a valid positive integer.")
+    positive_number = input("Please enter a valid positive integer: \n")
+
+i = int(positive_number) + 0
+while i >=1: 
+    print(i)
+    i -= 1
+
+#3
+i  = 1
+while i <= 100:
+    if i%3 == 0 and i%5 != 0:
+        print("Fizz")
+    elif i%5 == 0 and i%3 != 0:
+        print("Buzz")
+    elif i%3 == 0 and i%5 == 0:
+        print("FizzBuzz")
+    else:
+        print(i)
+    i += 1 
+
+#4 
+user_integer = int(input("What number would you like to go up to? "))
+print(f"\nHere is your table!\n")
+print(f"number | squared | cubed")
+print(f"------ | ------- | -----")
+
+for i in range (1, user_integer + 1):
+    print(f"{i:<7}|{i**2:<8} |{i**3:<4}")
+    go_up = input("Would you like to continue? ")
+    if go_up not in ['yes']:
+        break
+
+#5
+go_up = True
+while go_up:
+    your_grade = int(input("Please enter your numerical grade: "))
+    if your_grade <= 100 and your_grade >= 99:
+        print("A+")
+    elif your_grade <= 98 and your_grade>= 90: 
+        print("A")
+    elif your_grade <= 89 and your_grade>= 88: 
+        print("A-")
+    elif your_grade <= 87 and your_grade>= 86: 
+        print("B+")
+    elif your_grade <= 85 and your_grade>= 82: 
+        print("B")
+    elif your_grade <= 81 and your_grade>= 80: 
+        print("B-")
+    elif your_grade <= 79 and your_grade>= 78: 
+        print("C+")
+    elif your_grade <= 77 and your_grade>= 68: 
+        print("C")
+    elif your_grade <= 68 and your_grade>= 67: 
+        print("C-")
+    elif your_grade <= 66 and your_grade>= 65: 
+        print("D+")
+    elif your_grade <= 64 and your_grade>= 62: 
+        print("D")
+    elif your_grade <= 61 and your_grade>= 60: 
+        print("D-")
+    elif your_grade <= 55 and your_grade >= 0:
+        print("F")
+    go_up = input("Would you like to keep going? ")
+    if go_up not in ['yes']:
+        go_up = False
+
+#6
+books = [
+    {
+    "Title": "Gone Girl", 
+    "Author" : "Gillian Flynn",
+    "Genre": "Thriler"
+        },
+    {
+    "Title": "The Girl With the Dragon Tattoo",
+    "Author": "Stieg Larsson",
+    "Genre": "Mystery"
+    }, 
+    {
+    "Title": "The Silent Patient",
+    "Author": "Alex Michaelides",
+    "Genre": "Slow Burn"
+    }
+]
+
+your_genre = input("Please input the genre you want to read. The options are Thriller, Mystery, or Slow Burn:  ")
+for book in books: 
+    if your_genre == book["Genre"]:
+        print(book)
