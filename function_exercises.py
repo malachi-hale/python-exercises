3ab385e939adb9e3fcce63ded74aff4b4616b229
malachi-hale <malachi.d.hale@gmail.com>
2021-07-13 11:09:47 -0500

    functions exercises

--- function_exercises.py
+++ function_exercises.py
@@ -41,9 +41,9 @@ def capitalize(n):
         elif n[0] in 'aeiou':
             return n
     elif type(n) != str:
-        return print(n, "is not a word"), n 
+        return "not a word" 
     else:
-        return print(n, "is not a single word"), n 
+        return "not a single word"
 
 print(capitalize("bananas"))
 print(capitalize("apple"))
@@ -55,10 +55,10 @@ print("\n")
 #5
 def calculate_tip(tip, bill):
     if type(tip) == str or type(bill) == str:
-        return print("Not a number")
+        return "Not a number"
     elif type(tip) == float or int and type(bill)==float or int:
         if tip < 0 or tip > 1:
-            return print("This tip amount is not between 0 and 1.")
+            return "This tip amount is not between 0 and 1."
         elif tip >= 0 and tip <= 1:
            return tip * bill 
     else: 
@@ -66,4 +66,101 @@ def calculate_tip(tip, bill):
 print(calculate_tip(0.2, 100))
 print(calculate_tip(1.1, 60))
 print(calculate_tip("potatoes", "bananas"))
+print("\n")
+#6 
+def apply_discount(price, discount):
+    if type(price) == str or type(discount) == str:
+        return "Not a number"
+    elif type(price) == float or int and type(discount)==float or int:
+        return (price) - (price * discount)
 
+print(apply_discount(100, 0.2))
+print(apply_discount(60, 0.5))
+print(apply_discount("potatoes", "bananas"))
+print("\n")
+
+#7
+def handle_commas(number):   
+    if type(number) == str: 
+         new_number = number.replace(',', "")
+         return new_number 
+    elif type(number) == float or int:
+        return number
+
+print(handle_commas("9,000"))
+print(handle_commas("8,000,000"))
+print(handle_commas(9000))
+print(handle_commas("potatoes"))
+print("\n")
+
+#8 
+def get_letter_grade(grade):
+    if type(grade) == str:
+        return "Not a valid number grade."
+    elif type(grade) == float or int:
+        if grade <= 100 and grade >= 90:
+            return "A"
+        elif grade <= 89 and grade >= 80:
+            return "B"
+        elif grade <= 79 and grade >= 70:
+            return "C"
+        elif grade <= 69 and grade >= 60:
+            return "D"
+        elif grade <= 59:
+            return "F"
+   
+
+print(get_letter_grade(99.9))
+print(get_letter_grade(70))
+print(get_letter_grade(0))
+print(get_letter_grade("bananas"))
+print("\n")
+
+#9
+def remove_vowels(word):
+    if type(word) == str:
+        for i in word.lower():
+            if i in ('a', 'e', 'i', 'o', 'u'):
+                word=word.replace(i, "")
+        return word
+    elif type(word) == int or float: 
+        return "not a valid word"
+
+print(remove_vowels("bananas"))
+print(remove_vowels("Today I went to the mall and saw my friend"))
+print(remove_vowels(9000))
+
+#10 
+def normalize_name(name):
+    if type(name) == str:
+        name = name.lower()
+        for i in name:          
+            if i.isalpha() == False and i.isnumeric() == False:
+                name = name.replace(i, " ") 
+        name = name.strip()
+        name = name.replace(" ", "_")
+        while name[0].isnumeric():
+            name = name[1:]
+        return name
+    elif type(name) == float or int:
+        return "not a string"
+
+print(normalize_name("Today I went to the store"))
+print(normalize_name("%      words hello"))
+print(normalize_name("Today I % $"))
+print(normalize_name("999999%9_a9asdlk"))
+print(normalize_name(9000))
+print("\n")
+
+#11
+def cumulative_sum(list):
+    cumulative_list = []
+    i =0 
+    for j in range(0, len(list)):
+        i += list[j]
+        cumulative_list.append(i)
+    return cumulative_list
+
+list = [10, 20, 30, 40, 50]
+print(cumulative_sum(list))
+print("\n")
