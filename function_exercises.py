365991d96ecad83fb8f7bd2e61bf711fd5a42c29
malachi-hale <malachi.d.hale@gmail.com>
2021-07-12 19:56:38 -0500

    functions exercises

--- function_exercises.py
+++ function_exercises.py
@@ -35,15 +35,35 @@ print("\n")
 
 #4
 def capitalize(n):
-    if type(n) == str:
+    if type(n) == str and n.isalpha() == True:
         if n[0] in 'bcdfghjklmnpqrstvwxyz':
             return n.capitalize()
         elif n[0] in 'aeiou':
             return n
+    elif type(n) != str:
+        return print(n, "is not a word"), n 
     else:
-        return print("not a word")
+        return print(n, "is not a single word"), n 
 
 print(capitalize("bananas"))
 print(capitalize("apple"))
 print(capitalize("hello"))
 print(capitalize(9))
+print(capitalize("I love potatoes"))
+print("\n")
+
+#5
+def calculate_tip(tip, bill):
+    if type(tip) == str or type(bill) == str:
+        return print("Not a number")
+    elif type(tip) == float or int and type(bill)==float or int:
+        if tip < 0 or tip > 1:
+            return print("This tip amount is not between 0 and 1.")
+        elif tip >= 0 and tip <= 1:
+           return tip * bill 
+    else: 
+        return print("Invalid")
+print(calculate_tip(0.2, 100))
+print(calculate_tip(1.1, 60))
+print(calculate_tip("potatoes", "bananas"))
+
